18c8244b20776ed78eacea4c66c82dbea5f339b4
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-26 11:33:24 +0200

    Add project files.

---
+++ BasicOOP2026/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicOOP2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
