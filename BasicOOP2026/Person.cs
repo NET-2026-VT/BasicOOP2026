392623e9baeae98536d2a5133c658e38c1021853
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-26 11:54:42 +0200

    Added Person and its derived classes

---
+++ BasicOOP2026/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicOOP2026
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty; 
+    }
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+    internal class Admin : Employee
+    {
+        public string Department { get; set; } = string.Empty;        
+    }
+}
--- BasicOOP2026/Program.cs
+++ BasicOOP2026/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Part1();
+        }
+
+        private static void Part1()
+        {
+            Person p = new Person();
+            p.Name = "Kalle";
+            //p.Salary = 25000;
+            Employee e = new Employee();
+            e.Name = "Anna";
+            e.Salary = 25000;
+            //e.Department = "HR"; 
+            Admin a = new Admin();
+            a.Name = "Kim";
+            a.Salary = 35000;
+            a.Department = "IT";
+
+            Employee admin = new Admin();
+            Person admin2 = new Admin();
+            Admin admin3 = new Admin();
+
+            Do(p);
+            Do(e);
+            Do(a);
+            Do(admin);
+            Do(admin2);
+            Do(admin3);
+
+            List<Person> people = new List<Person>
+            {
+                p,
+                e,
+                a,
+                admin,
+                admin2,
+                admin3
+            };
+        }
+
+        private static void Do(Person p)
+        {
+            Console.WriteLine(p);
         }
     }
 }
